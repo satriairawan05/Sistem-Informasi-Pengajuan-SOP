6060af9729ffbfeb6058fb3f8986e9b06cecce7d
satriairawan05 <satriai418@gmail.com>
2024-01-23 08:44:03 +0800

    Update watermark.blade.php

--- resources/views/admin/partials/watermark.blade.php
+++ resources/views/admin/partials/watermark.blade.php
@@ -1,8 +1,8 @@
 <footer class="main-footer">
     <div class="footer-left">
-        Copyright &copy; 2023 - {{ date('Y') }} <div class="bullet"></div> Distributed by <span class="text-primary">AKG</span>
+        Copyright &copy; 2023 - {{ date('Y') }} <div class="bullet"></div> Distributed by <a href="https://alamkaryagemilang.com/" class="text-primary text-decoration-none">AKG</a>
     </div>
     <div class="footer-right">
-        All right reserved by <a href="https://www.linkedin.com/in/satriai418/" class="text-decoration-none">Nova</a>
+        All right reserved by <a href="https://samaricode.my.id/" class="text-decoration-none">Nova</a>
     </div>
 </footer>
